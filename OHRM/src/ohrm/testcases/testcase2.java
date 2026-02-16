802a3769d297492017649818d81474bf529062ab
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2026-02-16 09:13:33 +0530

    TestCase2 updated..

--- OHRM/src/ohrm/testcases/testcase2.java
+++ OHRM/src/ohrm/testcases/testcase2.java
@@ -4,7 +4,9 @@ public class testcase2 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("testcases2");
+		System.out.println("This is testcases2");
+		System.out.println("TestCase 2 updated...");
+		
 
 	}
 
